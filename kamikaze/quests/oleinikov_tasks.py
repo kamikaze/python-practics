42ae03c6a1570a54a34b3f89427e6e594693c879
Oleg Korsak <tigersoft.dg@gmail.com>
2012-01-22 15:18:36 +0200

    Added Task 2 implementation in php (made by Mike A). Reduced code size for python implementation. Added README file.
    Signed-off-by: Oleg Korsak <tigersoft.dg@gmail.com>

--- kamikaze/quests/oleinikov_tasks.py
+++ kamikaze/quests/oleinikov_tasks.py
@@ -9,30 +9,13 @@ Created on 21.01.2012
 from operator import itemgetter
 from itertools import groupby
 
-def __get_ranges(seq):
-    sequence_generator = (
-                          list(map(itemgetter(1), g)) for k, g in
-                          groupby(enumerate(sorted(seq)), lambda t: t[0]-t[1])
-                          )
-
-    for range_ in sequence_generator:
-        if range_[0] == range_[-1]:
-            yield str(range_[0])
-        else:
-            yield '%s-%s' % (range_[0], range_[-1])
-
 def numbersarr2range(numbers):
-    return '; '.join(__get_ranges(numbers))
+    ranges = [list(map(itemgetter(1), g)) for k, g in groupby(enumerate(sorted(numbers)), lambda t: t[0]-t[1])]
+    ranges = [str(range_[0]) if range_[0] == range_[-1] else '%s-%s' % (range_[0], range_[-1]) for range_ in ranges]
+    return '; '.join(ranges)
 
-def __get_numbers(range_str):
-    for range_ in range_str.split(';'):
-        limits = range_.strip().split('-', 1)
-        
-        try:
-            for number in range(int(limits[0]), int(limits[-1])+1):
-                yield number
-        except ValueError:
-            pass
 
 def ranges2numbersarr(range_str):
-    return [number for number in __get_numbers(range_str)]
+    ranges = [range_.split('-', 1) for range_ in range_str.split(';')]
+    ranges = [range(int(limit[0]), int(limit[-1])+1) for limit in ranges]
+    return [number for numbers in ranges for number in numbers]
