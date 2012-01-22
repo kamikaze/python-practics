9f1810861713e8c35d916963ea0270ed2b62d969
Julija Vinnik <jullija.vinnik@gmail.com>
2012-01-22 17:08:30 +0200

    Changed variable name
    Signed-off-by: Julija Vinnik <jullija.vinnik@gmail.com>

--- julia/stage0.py
+++ julia/stage0.py
@@ -4,34 +4,33 @@ def main():
     # Privetstvie programmi
     print('Hello, I am your first programm!')
     # Znakomstvo s poljzovatelem i peremennaja, kotoruju vvodit poljzovatelj, v dannom sluchae imja
-    yourName = input('What is your name?')
+    user_name = input('What is your name: ')
     # Statement (ne znaju kak nazvatj, chto-to vrode konstanti)
     name = 'Oleg'
     # Uslovie, sravnenie vvedennoj peremennoj so Statementom
-    if yourName == name:
+    if user_name == name:
         # Rezuljtat, kotorij programma vivodit na ekran, pri vipolnenii visheupomjanutogo uslovija
-        print('Nice to meet you, ' + yourName + ', Have a nice day!!!')
+        print('Nice to meet you, ' + user_name + ', Have a nice day!!!')
     # Uslovie, pri kotorom vvedennaj peremennaja ne sovpadaet so Statementom
     else:
         # Rezuljtat, kotorij programma vivodit na ekran, pri vipolnenii visheukazannogo uslovija
-        print('Nice to meet you, ' + yourName + '!')
-    # Prosjba programmi vvesti chislo
-    print(yourName + ', enter the number please!')
-    # Peremennaja, kotoruju vvodit poljzovatelj, v dannom sluchae chislo
-    num = input()
+        print('Nice to meet you, ' + user_name + '!')
+    # Prosjba programmi vvesti chislo i peremennaja, kotoruju vvodit poljzovatelj, v dannom sluchae chislo
+    num = input(user_name + ', enter the number please: ')
     # Statement
-    myNumber = 20
-    yourNumber = int(num)
+    my_number = 20
+    user_number = int(num)
     # Uslovie
-    if yourNumber > myNumber:
+    if user_number > my_number:
        # Rezuljtat na ekrane, pri vipolnenii visheukazannogo uslovija
        print('This number is greater than 20!')
     # Uslovie
-    elif yourNumber < myNumber:
+    elif user_number < my_number:
         # Rezuljtat na ekrane, pri vipolnenii visheukazannogo uslovija
         print('This number is less than 20!')
     else:
         print('This is exactly 20!')
+
 
 #detecting if file is called
 if __name__ == '__main__':
