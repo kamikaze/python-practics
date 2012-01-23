633a19ffe1fa888ef9f2094b2b9f88bf49438e81
Julija Vinnik <jullija.vinnik@gmail.com>
2012-01-23 23:34:52 +0200

    Fixed Issue #4
    Signed-off-by: Julija Vinnik <jullija.vinnik@gmail.com>

--- julia/stage1.py
+++ julia/stage1.py
@@ -1,26 +1,15 @@
-﻿#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      Julija
-#
-# Created:     23/01/2012
-# Copyright:   (c) Julija 2012
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 def main():
-    x = input('Please input a word (enter \"end\" to finish): ')
-    print (x)
     stop_word = 'end'
-    while stop_word != x:
-        input('Please input a word (enter \"end\" to finish): ')
+    x = total = input('Please input a word (enter "end" to finish): ')
 
-        if x == stop_word:
-            break
-            print('You have entered such words: ')
+    while x != stop_word:
+        x = input('Please input a word (enter "end" to finish): ')
+        total = total + ', ' + x
+
+    print('You have entered such words: ' + total)
 
 if __name__ == '__main__':
     main()
