662bc243b8b7c9ecafafe97724fc289ac9aec3cb
Julija Vinnik <jullija.vinnik@gmail.com>
2012-01-25 00:18:27 +0200

    Closing Issue #5
    Signed-off-by: Julija Vinnik <jullija.vinnik@gmail.com>

--- julia/stage2.py
+++ julia/stage2.py
@@ -1,37 +1,31 @@
 ﻿#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def main():
-    import random
+import random
 
+def main():
     guesses_taken = 0
-    print('Hello! What is your name?')
-    user_name = input()
+    user_name = input('Hello! What is your name?: ')
 
     number = random.randint(1, 20)
     print('Well, ' + user_name + ' I am thinking of a number between 1 and 20.')
 
     while guesses_taken < 6:
-        print('Take a guess.')
-        guess = input()
-        guess = int(guess)
+        guess = int(input('Take a guess: '))
 
         guesses_taken = guesses_taken + 1
 
         if guess < number:
-            print('Your guess is too low.')
-
-        if guess > number:
-            print('Your guess is too high.')
-
-        if guess == number:
+           print('Your guess is too low.')
+        elif guess > number:
+           print('Your guess is too high.')
+        else:
             break
 
     if guess == number:
         guesses_taken = str(guesses_taken)
         print('Good job, ' + user_name + '! You guessed my number in ' + guesses_taken + ' guesses!')
-
-    if guess != number:
+    else:
         number = str(number)
         print('Nope. The number I was thinking of was ' + number)
 
