8bc97e405bbe9c934e97b44ff5a584b82a65f3c1
Julija Vinnik <jullija.vinnik@gmail.com>
2012-01-28 16:40:17 +0200

    Fixed Issue #8
    Signed-off-by: Julija Vinnik <jullija.vinnik@gmail.com>

---
+++ julia/jokes.py
@@ -0,0 +1,17 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main():
+    input('What do you get when you cross a snowman with a vampire: ')
+    print('Frostbite!')
+    print()
+    input('What do dentists call a astronaut\'s cavity: ')
+    print('A black hole!')
+    print()
+    input('Knock knock.: ')
+    input("Who's there?")
+    input('Interrupting cow.')
+    print('Interrupting cow wh-MOO!')
+
+if __name__ == '__main__':
+    main()
